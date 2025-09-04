7865235893549bb61de6ff1b8bbb3e24bfedde68
Adnan Zaki <adnzaki@outlook.com>
2025-09-04 16:05:56 +0700

    fix missing navigation URL

--- app/Views/layout/nav.php
+++ app/Views/layout/nav.php
@@ -1,9 +1,9 @@
 <div class="classynav">
     <ul>
         <li><a href="<?= base_url(); ?>">Home</a></li>
-        <li><a href="#">Coding</a></li>
-        <li><a href="#">Tekno</a></li>
-        <li><a href="#">Puisi</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="<?= base_url('posts/category/coding'); ?>">Coding</a></li>
+        <li><a href="<?= base_url('posts/category/tekno'); ?>">Tekno</a></li>
+        <li><a href="<?= base_url('posts/category/puisi'); ?>">Puisi</a></li>
+        <li><a href="mailto:vuedevo@gmail.com">Contact</a></li>
     </ul>
 </div>
