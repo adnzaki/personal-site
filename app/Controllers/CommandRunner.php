570a67a3080045befcb08bf62012ce42bb9c7b5a
Adnan Zaki <adnzaki@outlook.com>
2025-09-04 11:50:19 +0700

    add command runner

---
+++ app/Controllers/CommandRunner.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers;
+
+class CommandRunner extends BaseController
+{
+    public function run()
+    {
+        $command = $this->request->getPost(['command', 'username', 'password']);
+        if ($command['username'] === env('dev_username') && $command['password'] === env('dev_password')) {
+            return $this->response->setJSON(command($command['command']));
+        } else {
+            return $this->response->setJSON(['status' => 'failed', 'message' => 'Invalid username or password']);
+        }
+    }
+}
