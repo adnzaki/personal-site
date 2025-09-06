50f684394414d29b1313e69f5cfbb5f3e3a05100
Adnan Zaki <adnzaki@outlook.com>
2025-09-06 23:20:08 +0700

    add new category: "Nurani"

--- app/Views/layout/nav.php
+++ app/Views/layout/nav.php
@@ -3,6 +3,7 @@
         <li><a href="<?= base_url(); ?>">Home</a></li>
         <li><a href="<?= base_url('posts/category/coding'); ?>">Coding</a></li>
         <li><a href="<?= base_url('posts/category/tekno'); ?>">Tekno</a></li>
+        <li><a href="<?= base_url('posts/category/nurani'); ?>">Nurani</a></li>
         <li><a href="<?= base_url('posts/category/puisi'); ?>">Puisi</a></li>
         <li><a href="mailto:vuedevo@gmail.com">Contact</a></li>
     </ul>
