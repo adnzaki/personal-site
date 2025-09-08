0bfa47e0b0841821e9d00e5701ee511f659fa30a
Adnan Zaki <adnzaki@outlook.com>
2025-09-08 11:24:06 +0700

    remove unused method

--- app/Controllers/PostTrait.php
+++ app/Controllers/PostTrait.php
@@ -21,21 +21,4 @@ trait PostTrait
             $this->model->where('post_id', $postId)->set('views', $detail['views'] + 1)->update();
         }
     }
-
-    public function popularPosts()
-    {
-        $ids = [];
-        $topPosts = $this->model->orderBy('views', 'desc');
-
-        if ($topPosts->countAllResults(false) > 0) {
-            $ids = $topPosts->findAll(5);
-            $ids = array_column($ids, 'post_id');
-            $popularPosts = wp()->setPerPage(5)
-                            ->setOrder('include')
-                            ->setIds($ids)
-                            ->getPosts(1, '', '', '')['data'];
-        }
-
-        return $popularPosts ?? [];
-    }
 }
